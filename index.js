e3c7b1a838848f32c8638483dbdc67848dc78a78
Collin Edwards <collinedwards1579@gmail.com>
2018-01-20 22:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var recipes = {Gungus: "3 Heaping Spoons of Gungus Powder"}
 function updateObjectWithKeyAndValue(){
   recipes.Hungulus = "Sprungulus' Delight!"
   Object.assign({}, recipes, {Dirt: "Just find some outside, alright?"})
+  return recipes
 }
