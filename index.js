39ee6e10f6a8acbc69f23cf7af7262ce6f4efd29
Collin Edwards <collinedwards1579@gmail.com>
2018-01-20 22:59:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {Gungus: "3 Heaping Spoons of Gungus Powder"}
 
 function updateObjectWithKeyAndValue(){
+  delete recipes.Gungus
   recipes.prop = 1
   recipes.prop1 = 2
   return recipes
