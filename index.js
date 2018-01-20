b4263d54afd10c42ab6bcf21e152486892c10beb
Collin Edwards <collinedwards1579@gmail.com>
2018-01-20 23:03:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var recipes = {Gungus: "3 eaping Spoons of Gungus Powder-- That's it!"}
 
 function updateObjectWithKeyAndValue(){
   delete recipes.Gungus
-  Object.assign({}, recipes, {prop: 1})
-  return Object.assign({}, recipes, {prop2: 2})
+  return Object.assign({}, recipes, {prop: 1}, {prop2: 2})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(){
